f2f497454a466901329f258e96040b34fcbd96db
gzwnice <gzwnice@163.com>
2019-11-08 14:49:42 +0800

    fix: first init

---
+++ src/components/UserPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{msg }} {{ raw }}</h1>
+    <h1 v-html="raw"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage',
+  data () {
+    return {
+      msg: 'user page is me',
+      raw: '<h3>vue-Jayce</h3>'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
